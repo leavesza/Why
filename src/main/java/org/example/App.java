2bc21cca51f99e909345ca74c40073b3795b35bd
WilliamHenryKing <kingiamwill@gmail.com>
2020-02-18 18:43:04 +0200

    Created code to generate a hundred random numbers and print it out. Also added code to identify the largest number.

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,38 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        int[] randomNumbers = IntStream.generate(
+                                () -> ThreadLocalRandom.current().nextInt(0, 10000 + 1))
+                                        .limit(100)
+                                        .boxed()
+                                        .mapToInt(i -> i)
+                                        .toArray();
+
+        int largestNumber = Arrays.stream(randomNumbers)
+                                    .max()
+                                    .getAsInt();
+
+        int smallestNumber = Arrays.stream(randomNumbers)
+                                    .min()
+                                    .getAsInt();
+
+        System.out.println("Printing the whole array...");
+        Arrays.stream(randomNumbers)
+                .forEach(System.out::println);
+
+        System.out.println("\n\nThe largest number is: " + largestNumber);
+
+
+
+
+    }
+}
